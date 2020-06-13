ea0def281157c05302c342eacc0da98514e153e5
Mukesh Maurya <mukesh@insider.in>
2020-06-13 23:31:12 +0530

    removed component folder

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import React from 'react';
+import React from "react";
 
-import logo from './logo.svg';
-import './App.css';
-import Task from './components/Task';
+import logo from "./logo.svg";
+import "./App.css";
+import Task from "./components/Task";
 
 function App() {
   return (
-    <Task />
+    //
+    Hello
   );
 }
 
