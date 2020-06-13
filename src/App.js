a6f6de0dec8ed65efe09d20a586fe7f960b8d8c5
Mukesh Maurya <mukesh@insider.in>
2020-06-13 23:39:12 +0530

    removed import of task component from app.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 
 import logo from "./logo.svg";
 import "./App.css";
-import Task from "./components/Task";
+// import Task from "./components/Task";
 
 function App() {
   return (
